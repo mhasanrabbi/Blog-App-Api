b0c43c3c70a8de73d1a1471182ed90856b03eb13
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-12-05 14:56:39 +0600

    login register routes

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -12,6 +14,8 @@ mongoose
   })
   .then(console.log('Connected to MongoDB'))
   .catch((err) => console.log(err));
+
+app.use('/api/auth', authRoute);
 
 app.listen('5000', () => {
   console.log('Backend is run');
